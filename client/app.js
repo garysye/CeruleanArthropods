7e3560b09ae088f83395af458d8be28898fe07d6
amy-chiu <amy.lee.chiu@gmail.com>
2015-09-24 19:32:52 -0700

    Sets up initial angular files and html files

---
+++ client/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('eir', ['eir.home', 'ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/home', {
+    templateUrl:
+    controller:
+  })
+  // .when('/about', {
+  //   templateUrl:
+  //   controller:
+  // })
+  // .when('/patients', {
+  //   templateUrl:
+  //   controller:
+  // })
+  // .when('/donation', {
+  //   templateUrl:
+  //   controller:
+  // })
+  // .when('/signup', {
+  //   templateUrl:
+  //   controller:
+  // })
+  
+  // .when('/login', {
+  //   templateUrl:
+  //   controller:
+  // })
+});
