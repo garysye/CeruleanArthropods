ec1230af71de9059119cfe8ce4d8037639814c6e
Gary Ye <garysye@gmail.com>
2015-09-25 15:00:55 -0700

    Sets up routes for donations and conditions
    Uses path urls "/classes/conditions" and "/classes/donations"
    as API endpoints for conditions and donations, respectively.

---
+++ server/conditions/conditionRoutes.js
@@ -0,0 +1,10 @@
+//var conditionController = require() TODO: complete this when controller is built
+
+module.exports = function (router) {
+
+//Routes requests to specific controller methods
+router.route('/')
+//TODO: uncomment these when patient controller is built
+  // .get(conditionController.getConditions)
+
+}
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,14 +4,21 @@ var bodyParser = require('body-parser');
 module.exports = function (app, express) {
 
   var patientRouter = express.Router();
+  var donationRouter = express.Router();
+  var conditionRouter = express.Router();
 
 //Serve up static files in client folder and other middleware
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/client'));
 
-//Routes patients traffic to patientRouter and injects the router into its route file
+//Routes traffic to their router and injects the router into its route file
   app.use('/classes/patients', patientRouter);
   require('../patients/patientRoutes')(patientRouter)
 
+  app.use('/classes/donations', donationRouter);
+  require('../donations/donationRoutes')(donationRouter);
+
+  app.use('/classes/conditions', conditionRouter);
+  require('../conditions/conditionRoutes')(conditionRouter);
 }
