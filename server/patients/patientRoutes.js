13681356bc290ebea3216c5facd07236eca7657d
taylorleh <taylorleh@mac.com>
2015-09-28 14:24:54 -0700

    adds patient endpoints and api

--- server/patients/patientRoutes.js
+++ server/patients/patientRoutes.js
@@ -1,11 +1,13 @@
-//var patientController = require() TODO: complete this when controller is built
+// TODO: complete this when controller is built
+var patientController = require('./patientController.js');
 
 module.exports = function (router) {
 
 //Routes requests to patientController
-router.route('/')
 //TODO: uncomment these when patient controller is built
-  // .get(patientController.getPatients)
-  // .post(patientController.postPatient)
 
-}
+router.route('/')
+  .get(patientController.getPatients)
+  .post(patientController.addNewPatient);
+
+};
