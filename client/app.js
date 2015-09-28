c78ef3357c6a4d38a050d4f8382b999fe743b14a
amy-chiu <amy.lee.chiu@gmail.com>
2015-09-28 15:11:28 -0700

    changes to patients file, uploads bootstrap stuff, etc

--- client/app.js
+++ client/app.js
@@ -1,4 +1,14 @@
-var app = angular.module('eir', ['eir.home', 'eir.about', 'eir.patients', 'eir.getFunded', 'eir.patientsID', 'eir.donateID','eir.thankYou', 'ngRoute']);
+var app = angular.module('eir', [
+  'eir.home', 
+  // 'eir.about', 
+  'eir.patients', 
+  // 'eir.getFunded', 
+  // 'eir.patientsID', 
+  // 'eir.donateID',
+  // 'eir.thankYou', 
+  // 'ui.bootstrap', 
+  'ngRoute']
+);
 
 app.config(function($routeProvider) {
   $routeProvider
@@ -6,30 +16,30 @@ app.config(function($routeProvider) {
     templateUrl: '/home/home.html',
     controller: 'homeCtrl'
   })
-  .when('/about', {
-    templateUrl: '/about/about.html'
-    controller: 'aboutCtrl'
-  })
+  // .when('/about', {
+  //   templateUrl: '/about/about.html',
+  //   controller: 'aboutCtrl'
+  // })
   .when('/patients', {
-    templateUrl: '/patients/patients.html'
+    templateUrl: '/patients/patients.html',
     controller: 'patientsCtrl'
   })
-  .when('/get-funded', {
-    templateUrl: '/patients/get-funded.html'
-    controller: 'getFundedCtrl'
-  })
-  .when('/patients/:id', {
-    templateUrl: '/patients/:id.html'
-    controller: 'patientProfileCtrl'
-  })
-  .when('/donate/:id', {
-    templateUrl: '/donate/:id.html'
-    controller: 'donateCtrl'
-  })
-  .when('/thank-you', {
-    templateUrl: '/thank-you.html'
-    controller: 'thankYouCtrl'
-  })
+  // .when('/get-funded', {
+  //   templateUrl: '/patients/get-funded.html',
+  //   controller: 'getFundedCtrl'
+  // })
+  // .when('/patients/:id', {
+  //   templateUrl: '/patients/:id.html',
+  //   controller: 'patientProfileCtrl'
+  // })
+  // .when('/donate/:id', {
+  //   templateUrl: '/donate/:id.html',
+  //   controller: 'donateCtrl'
+  // })
+  // .when('/thank-you', {
+  //   templateUrl: '/thank-you.html',
+  //   controller: 'thankYouCtrl'
+  // })
   .otherwise({ 
     redirectTo: '/home' 
   });
---
+++ client/patients/patients.js
@@ -0,0 +1,19 @@
+angular.module('eir.patients', [])
+
+.controller('patientsCtrl', function($scope, $http, $routeParams) {
+
+  // $scope.getPatients = function() {
+
+  //   $http.get('/classes/patients')
+  //   .success()
+  //   .error()
+
+
+  // };
+
+  // $scope.getPatients();
+
+
+
+
+});
