0eb62b16ddd374315b92ba7760a0750e6c4fc486
Benjamin <bhoffman8@gmail.com>
2015-09-29 13:09:34 -0700

    adds routing for app.js

--- client/app.js
+++ client/app.js
@@ -3,11 +3,11 @@ var app = angular.module('eir', [
   'eir.home', 
   'eir.about', 
   'eir.patients', 
-  // 'eir.getFunded', 
-  // 'eir.patientsID', 
+  'eir.getFunded', 
+  'eir.profile', 
   'eir.donate',
-  // 'eir.thankYou', 
-  // 'ui.bootstrap', 
+  'eir.thankYou', 
+  'ui.bootstrap', 
   'ngRoute']
 );
 
@@ -25,22 +25,22 @@ app.config(function($routeProvider) {
     templateUrl: '/patients/patients.html',
     controller: 'patientsCtrl'
   })
-  // .when('/get-funded', {
-  //   templateUrl: '/patients/get-funded.html',
-  //   controller: 'getFundedCtrl'
-  // })
-  // .when('/patients/:id', {
-  //   templateUrl: '/patients/:id.html',
-  //   controller: 'patientProfileCtrl'
-  // })
+  .when('/get-funded', {
+    templateUrl: '/patients/get-funded.html',
+    controller: 'getFundedCtrl'
+  })
+  .when('/profile', {
+    templateUrl: '/profile/profile.html',
+    controller: 'profileCtrl'
+  })
   .when('/donate/:id', {
     templateUrl: '/donate/donate.html',
     controller: 'donateCtrl'
   })
-  // .when('/thank-you', {
-  //   templateUrl: '/thank-you.html',
-  //   controller: 'thankYouCtrl'
-  // })
+  .when('/thank-you', {
+    templateUrl: '/thank-you.html',
+    controller: 'thankYouCtrl'
+  })
   .otherwise({ 
     redirectTo: '/home' 
   });
