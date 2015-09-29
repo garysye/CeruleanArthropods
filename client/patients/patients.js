878dd20f0835f445c2ce901c3f5175612632dab5
amy-chiu <amy.lee.chiu@gmail.com>
2015-09-29 12:55:57 -0700

    screws more with patients file

--- client/patients/patients.js
+++ client/patients/patients.js
@@ -2,24 +2,25 @@ angular.module('eir.patients', [])
 
 .controller('patientsCtrl', function($scope, patientsFactory) {
 
-  $scope.patients = patientsFactory.patients;
 
-  // $scope.patients = {
-  //   patients: [],
-  //   getPatients: function() {}
-  //   
+  $scope.patients;
+
+  patientsFactory.getPatients()
+  .then(function(res){
+    console.log(res);
+    $scope.patients = res;
+  })
+
+  // $scope.leftToFund = function() {
+
+  //   for(var i = 0; i < $scope.patients.length; i++) {
+  //     leftToFund = $scope.patients[i].goal - $scope.patients[i].progress
+  //     $scope.patients[i].leftToFund = leftToFund;
+  //   }
+
   // }
 
-  $scope.leftToFund = function() {
-
-    for(var i = 0; i < $scope.patients.length; i++) {
-      leftToFund = $scope.patients[i].goal - $scope.patients[i].progress
-      $scope.patients[i].leftToFund = leftToFund;
-    }
-
-  }
-
-  $scope.leftToFund();
+  // $scope.leftToFund();
 
   
 
