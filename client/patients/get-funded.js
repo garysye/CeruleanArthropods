28d146de88276b0a75f146144489e8d612ac616f
Benjamin <bhoffman8@gmail.com>
2015-09-29 17:04:13 -0700

    Completes HTML form for new patients:Wq :wq!

--- client/patients/get-funded.js
+++ client/patients/get-funded.js
@@ -2,5 +2,11 @@ angular.module('eir.getFunded', [])
 
 .controller('getFundedCtrl', function ($scope, patientsFactory) {
 
-  
+  $scope.patient = {};
+
+  patientsFactory.submitPatientForm()
+
+  $scope.update = function(patient) {
+
+  };
 });
