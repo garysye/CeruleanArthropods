6ce24f6ea473bcc9e723f322e1f3759ce0d15840
Benjamin <bhoffman8@gmail.com>
2015-09-29 23:44:09 -0700

    Begins work on two html forms (donars & patients)

--- client/services/factory.js
+++ client/services/factory.js
@@ -1,7 +1,33 @@
 var app = angular.module('eir.factory', []);
 
+
+// this factory will hold all VERB requests relating to patients
 app.factory('patientsFactory', function ($http) {
 
+  // POST req; this allows patients to enter themselves on our website
+  var submitPatientForm = function(newPatient) {
+    return $http.post('/classes/patients/', newPatient)
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR submitPatientForm: ' + err);
+      });
+  };
+
+  // GET req; this will retrieve one specific patient from our db
+  var getPatient = function(id) {
+    return $http.get('/classes/patients/' + id)
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR getPatient: ' + err);
+      });
+
+  };
+
+  // GET req; this will retrieve all our patients from our db
   var getPatients = function() {
     return $http.get('/classes/patients')
       .then(function(res) {
@@ -12,67 +38,96 @@ app.factory('patientsFactory', function ($http) {
       });
   };
 
-  var getPatient = function(id) {
-    return $http.get('/classes/patients/' + id)
-      .then(function(res) {
-        return res.data
-      })
-      .catch(function(err) {
-        console.log('ERROR getPatient: ' + err);
-      });
-
-  };
-
-  var submitPatientForm = function(newPatient) {
-    return $http.post('/classes/patients/', newPatient)
-      .then(function(res) {
-        return res.data
-      })
-      .catch(function(err) {
-        console.log('ERROR submitPatientForm: ' + err)
-      })
-  };
-
-  // submitPatientForm
-    // post request -- patiendInfoObj
-    // send back all the info (includ id) in the post response
-      // on response, display the patient's info
-
-
-// donors factory (donorsFactory)
-  // submitDonationForm
-    // post request -- donationInfoObj
-    // send back all the info in the post request
-      // on response, display their donation info
-    // PUT request to update patients patient.progress
-    // send us back entire patient object
-    
-
-  // getDonors
-    // get request
-  // getDonor -- donor
-    // get request
-
-
-
-// condition factory (conditionFactory)
-  // getCondition -- conditionID
-    // get request
-
-
-
-
   return {
-    getPatients: getPatients,
-    getPatient: getPatient
-    // 
-    //
-    //
-    //
+    submitPatientForm: submitPatientForm,
+    getPatient: getPatient,
+    getPatients: getPatients
   }
 
+});
 
+
+
+app.factory('donorsFactory', function ($http) {
+
+  // POST req; this will allow donors to make their donation
+  var submitDonationForm = function (newDonation) {
+    return $http.post('/classes/donation', newDonation)
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR submitDonationForm: ' + err);
+      });
+  };
+
+  // PUT req; this will update the patients financial/donation progress
+  var updatePatientProgress = function (donationAmount) {
+    return $http.put('/classes/patients/' + id, donationAmount)
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR updatePatientProgress: ' + err);
+      });
+  };
+
+  // GET req; this will retrieve all the donors from our db
+  var getDonors = function() {
+    return $http.get('/classes/donors')
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR getDonors: ' + err);
+      });
+  };
+
+  // GET req; this will retrieve one specific donor from our db
+  var getDonor = function(id) {
+    return $http.get('/classes/donors/' + id)
+      .then(function(res) {
+        return res.data
+      })
+      .catch(function(err) {
+        console.log('ERROR getDonor: ' + err);
+      });
+  };
+
+  return {
+    submitDonationForm: submitDonationForm,
+    updatePatientProgress: updatePatientProgress,
+    getDonors: getDonors,
+    getDonor: getDonor
+  }
+
+});
+
+app.factory('conditionFactory', function ($http) {
+
+  // GET req; this will retrieve one specific condition from our db
+  var getCondition = function(id) {
+    return $http.get('classes/conditions' + id)
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR getCondition: ' + err);
+      });
+  };
+
+  // GET req; this will retrieve all the conditions from our db
+  var getConditions = function() {
+    return $http.get('classes/conditions')
+      .then(function(res) {
+        return res.data;
+      })
+      .catch(function(err) {
+        console.log('ERROR getCondition: ' + err);
+      });
+  };
   
-
-
+  return {
+    getCondition: getCondition
+  }
 });
