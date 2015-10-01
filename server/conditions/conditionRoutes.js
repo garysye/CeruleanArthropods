9b72e87d4dc2625b1b73f15a50eb5eda7f6d56f0
amy-chiu <amy.lee.chiu@gmail.com>
2015-10-01 16:30:22 -0700

    adds condition routes and controller

--- server/conditions/conditionRoutes.js
+++ server/conditions/conditionRoutes.js
@@ -1,10 +1,19 @@
-//var conditionController = require() TODO: complete this when controller is built
+var conditionController = require('./conditionsController.js');
 
 module.exports = function (router) {
 
 //Routes requests to specific controller methods
-router.route('/')
+// router.route('/')
 //TODO: uncomment these when patient controller is built
   // .get(conditionController.getConditions)
 
+//looks for "id" parameter and if there is a match 
+router.param('id', function (req, res, next, id){
+  req.id = id;
+  next();
+});
+
+
+router.route('/:id')
+  .get(conditionController.getConditionById)
 }
