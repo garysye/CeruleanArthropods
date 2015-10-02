d8b1c0a896ed04930b007d27269f1d65fa5ad23d
Benjamin <bhoffman8@gmail.com>
2015-10-02 00:40:49 -0700

    skeleton for first tests

---
+++ test/unit-tests/patientsFactoryTest.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+var assert = chai.assert;
+var should = chai.should();
+// console.log(__dirname)
+// var factory = require('../../client/services/factory.js')
+
+describe('the patientsFactory', function() {
+  describe('submitPatientForm', function() {
+    it('should exist', function() {
+      assert.typeOf(factory.patientsFactory.submitPatientForm, 'function', 'submitPatientForm method should exist');
+    });
+  });
+
+  describe('getPatients', function() {
+    it('should exist', function() {
+
+    });
+  });
+
+  describe('getPatient', function() {
+    it('should exist', function() {
+
+    });
+  });
+});
